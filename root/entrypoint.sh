a6e431c8c78ba4e3e520c956c61924e595071387
Emory Merryman <emory.merryman@deciphernow.com>
2017-03-25 12:18:48 +0000

    fixed licensing

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
 # This file is part of object-drive-ui-baker.
 #
-#    baker is free software: you can redistribute it and/or modify
+#    object-drive-ui-baker is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
