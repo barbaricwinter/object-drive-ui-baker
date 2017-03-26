1727735b52ea14c1494f99064a4bbb6c6e159c3c
Emory Merryman <emory.merryman@deciphernow.com>
2017-03-26 21:22:07 +0000

    use find not ls

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -122,7 +122,7 @@ TSTAMP=$(date -u) &&
         --rm \
         --volume ${FIREFOX_HOME}:/home/user:ro \
         alpine:3.4 \
-        ls /home/user/.mozilla/firefox/*.default/cert8.db) &&
+        find /home/user/.mozilla/firefox/ -name cert8.db) &&
     cat /opt/docker/cert8.db | docker \
         run \
         --interactive \
